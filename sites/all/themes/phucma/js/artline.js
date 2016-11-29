6fe62b8aa4f686e1bb79d89866b2b52a7b1670e3
Nhan Le <nhan.lq@mobivi.vn>
2016-11-29 08:12:41 +0700

    update

---
+++ sites/all/themes/phucma/js/artline.js
@@ -0,0 +1,87 @@
+(function ($) {
+
+    Drupal.behaviors.initDropzone = {
+        attach: function (context, settings) {
+            Dropzone.autoDiscover = false;
+            $("div#dropzone-upload").dropzone({url: "/uploader/post"});
+            $(".tabs-menu a").click(function (event) {
+                event.preventDefault();
+                $(this).parent().addClass("current");
+                $(this).parent().siblings().removeClass("current");
+                var tab = $(this).attr("href");
+                $(".tab-content").not(tab).css("display", "none");
+                $(tab).fadeIn();
+
+            });
+
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_minimal-blue',
+                radioClass: 'iradio_minimal-blue',
+                increaseArea: '20%' // optional
+            });
+            $('select').niceSelect();
+
+
+            $("#block-artline-artline-post a.inline").colorbox({inline: true, width: "375px", height: "500px"});
+
+
+            $(".post_bottom").click(function () {
+                $("#block-artline-artline-post a.inline").click();
+            })
+
+            $(".post-article-click").click(function () {
+                $("#block-artline-artline-post a.inline").click();
+            });
+            $("form#post-form-article-1").submit(function () {
+
+                var cate = $(this).find("select#category").val();
+
+                if ($.isNumeric(cate) == false) {
+                    $(this).find(".nice-select.category").addClass('error');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+            $("form#post-form-article-2").submit(function () {
+                var cate = $(this).find("select#category").val();
+                if ($.isNumeric(cate) == false) {
+                    $(this).find(".nice-select.category").addClass('error');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+
+
+            $(".name-store a").each(function () {
+
+                $(this).click(function () {
+                    //jQuery(".detail-store").hide();
+                    $(this).parent().next().toggle();
+                    return false;
+                });
+            });
+
+            $('select#location-selector').niceSelect();
+
+            $("#location-selector").change(function () {
+                $("ul.store .location").hide();
+                var tid_ = $(this).val();
+                $("ul.store li.location-" + tid_).show();
+            });
+
+
+               $('#view-content-ajax').NewWaterfall();
+
+            $("#block-artline-artline-category ul li ul li").each(function(){
+               if($(this).hasClass('active')){
+                   $(this).parent().parent().addClass('active');
+               }
+            });
+
+
+        }
+    };
+
+})(jQuery);
