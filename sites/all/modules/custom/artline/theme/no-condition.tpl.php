2892172ddc24dbe7cd99070c2991eda6dfca4aa2
Nhan Le <nhan.lq@mobivi.vn>
2016-12-13 09:39:38 +0700

    update

--- sites/all/modules/custom/artline/theme/no-condition.tpl.php
+++ sites/all/modules/custom/artline/theme/no-condition.tpl.php
@@ -9,6 +9,6 @@
 <div class="no-condition">
     <p><?php print t('Số điểm hiện nay bạn không đủ để đổi sản phẩm, bạn có thể bớt, thay đổi sản phẩm khác hoặc bạn có thể xem cách tích điểm bên dưới nhé.') ?></p>
 <div class="conditon-info">
-    <?php print variable_get('rule_game') ?>
+    <?php //print variable_get('rule_game') ?>
 </div>
 </div>
