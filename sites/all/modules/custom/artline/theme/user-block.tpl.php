717755dd0b63769511db8adc4204f87055062e93
Nhan Le <nhan.lq@mobivi.vn>
2016-12-25 16:16:12 +0700

    update

--- sites/all/modules/custom/artline/theme/user-block.tpl.php
+++ sites/all/modules/custom/artline/theme/user-block.tpl.php
@@ -26,9 +26,9 @@ if ($user->uid > 0) {
     <?php if ($user->uid > 0): ?>
         <li>
         <?php if ($account->picture): ?>
-            <span><img src="<?php print image_style_url('avatar', $account->picture->uri) ?>"><i class="notification">1</i></span>
+            <span><img src="<?php print image_style_url('avatar', $account->picture->uri) ?>"></span>
         <?php else: ?>
-            <span><img src="<?php print base_path() . drupal_get_path('theme', 'phucma') ?>/images/default-avatar.png"><i class="notification">1</i></span>
+            <span><img src="<?php print base_path() . drupal_get_path('theme', 'phucma') ?>/images/default-avatar.png"></span>
         <?php endif; ?>
     <?php else: ?>
         <span><img
