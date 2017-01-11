964d72f1e2d4f518a82bad258fc6964b892e64dd
Nhan Le <nhanlego1@gmail.com>
2017-01-11 11:15:00 +0700

    update

--- sites/all/themes/phucma/templates/html.tpl.php
+++ sites/all/themes/phucma/templates/html.tpl.php
@@ -60,7 +60,13 @@
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-7140912397982465",
+      enable_page_level_ads: true
+    });
+  </script>
 
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
