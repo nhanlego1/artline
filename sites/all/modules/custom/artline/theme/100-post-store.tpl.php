eea93edc5bc013aeb89a467719d2d2449383a569
Nhan Le <nhanlego1@gmail.com>
2017-03-17 22:36:41 +0700

    update

--- sites/all/modules/custom/artline/theme/100-post-store.tpl.php
+++ sites/all/modules/custom/artline/theme/100-post-store.tpl.php
@@ -9,12 +9,12 @@
 <div class="200-post-store" style="display: none;">
     <?php if ($nodes): ?>
         <?php foreach ($nodes as $node): ?>
-            <a class="ajax cboxElement ctools-use-modal-<?php print $node->nid ?>"
-               href="<?php print url('artline/store/' . $node->nid); ?>">Store <?php $node->nid ?></a>
-            <a class="ajax cboxElement product-ctools-use-modal-<?php print $node->nid ?>"
-               href="<?php print url('artline/product/' . $node->nid); ?>">product <?php $node->nid ?></a>
-            <a class="ajax cboxElement xu-ctools-use-modal-<?php print $node->nid ?>"
-               href="<?php print url('artline/xu/' . $node->nid); ?>">Store <?php $node->nid ?></a>
+            <a class="ajax cboxElement ctools-use-modal-<?php print $node ?>"
+               href="<?php print url('artline/store/' . $node); ?>">Store <?php $node ?></a>
+            <a class="ajax cboxElement product-ctools-use-modal-<?php print $node ?>"
+               href="<?php print url('artline/product/' . $node); ?>">product <?php $node ?></a>
+            <a class="ajax cboxElement xu-ctools-use-modal-<?php print $node ?>"
+               href="<?php print url('artline/xu/' . $node); ?>">Store <?php $node ?></a>
         <?php endforeach; ?>
     <?php endif; ?>
 </div>
